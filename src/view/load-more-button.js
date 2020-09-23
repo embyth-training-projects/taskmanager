147f0802e04dd3a5a443d3e1509a99bd1b9d43cc
Rostyslav Miniukov <thresh1337@gmail.com>
2020-09-23 18:25:53 +0300

    Разбил код на модули

---
+++ src/view/load-more-button.js
@@ -0,0 +1,6 @@
+// Функция создания шаблона кнопки "Load More"
+export const createLoadMoreButtonTemplate = () => {
+  return (
+    `<button class="load-more" type="button">load more</button>`
+  );
+};
