bc3715350c34dc2f30c21f2eb9dd3f0183c24e3c
Rostyslav Miniukov <thresh1337@gmail.com>
2020-10-17 22:09:03 +0300

    Реализуем добавление задачи через меню

--- src/main.js
+++ src/main.js
@@ -26,10 +26,16 @@ render(siteHeaderNode, siteMenuComponent, RenderPosition.BEFOREEND);
 const boardPresenter = new BoardPresenter(siteMainNode, filterModel, tasksModel);
 const filterPresenter = new FilterPresenter(siteMainNode, filterModel, tasksModel);
 
+const handleTaskNewFormClose = () => {
+  siteMenuComponent.getElement().querySelector(`[value=${MenuItem.TASKS}]`).disabled = false;
+  siteMenuComponent.setMenuItem(MenuItem.TASKS);
+};
+
 const handleSiteMenuClick = (menuItem) => {
   switch (menuItem) {
     case MenuItem.ADD_NEW_TASK:
-
+      boardPresenter.createTask(handleTaskNewFormClose);
+      siteMenuComponent.getElement().querySelector(`[value=${MenuItem.TASKS}]`).disabled = true;
       break;
     case MenuItem.TASKS:
 
@@ -44,8 +50,3 @@ siteMenuComponent.setMenuClickHandler(handleSiteMenuClick);
 
 filterPresenter.init();
 boardPresenter.init();
-
-document.querySelector(`#control__new-task`).addEventListener(`click`, (evt) => {
-  evt.preventDefault();
-  boardPresenter.createTask();
-});
