97faad0122c371002702447e54edbb1d3c6877e1
Rostyslav Miniukov <thresh1337@gmail.com>
2020-10-17 22:00:51 +0300

    Программируем меню приложения

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import TasksModel from './model/tasks';
 import FilterModel from './model/filter';
 import {generateTask} from './mock/task';
 import {render, RenderPosition} from './utils/render';
+import {MenuItem} from './const';
 
 // Константа количества карточек заданий
 const TASK_AMOUNT = 22;
@@ -18,11 +19,28 @@ const filterModel = new FilterModel();
 
 const siteMainNode = document.querySelector(`.main`);
 const siteHeaderNode = siteMainNode.querySelector(`.main__control`);
+const siteMenuComponent = new SiteMenuView();
 
-render(siteHeaderNode, new SiteMenuView(), RenderPosition.BEFOREEND);
+render(siteHeaderNode, siteMenuComponent, RenderPosition.BEFOREEND);
 
 const boardPresenter = new BoardPresenter(siteMainNode, filterModel, tasksModel);
 const filterPresenter = new FilterPresenter(siteMainNode, filterModel, tasksModel);
+
+const handleSiteMenuClick = (menuItem) => {
+  switch (menuItem) {
+    case MenuItem.ADD_NEW_TASK:
+
+      break;
+    case MenuItem.TASKS:
+
+      break;
+    case MenuItem.STATISTICS:
+
+      break;
+  }
+};
+
+siteMenuComponent.setMenuClickHandler(handleSiteMenuClick);
 
 filterPresenter.init();
 boardPresenter.init();
