b6ebfae6579a42b5d9885096d855b83dfaea8060
Rostyslav Miniukov <thresh1337@gmail.com>
2020-10-28 18:01:03 +0200

    Подключаем ServiceWorker

--- src/main.js
+++ src/main.js
@@ -67,3 +67,13 @@ api.getTasks()
     render(siteHeaderNode, siteMenuComponent, RenderPosition.BEFOREEND);
     siteMenuComponent.setMenuClickHandler(handleSiteMenuClick);
   });
+
+window.addEventListener(`load`, () => {
+  navigator.serviceWorker.register(`/sw.js`)
+      .then(() => {
+        console.log(`ServiceWorker available`); // eslint-disable-line
+      })
+      .catch(() => {
+        console.error(`ServiceWorker isn't available`); // eslint-disable-line
+      });
+});
